22e8a5dcac8c8a474f8cadb2b49f1062bff088c7
Daniel Parra <daniel27parra@gmail.com>
2025-09-24 13:30:01 -0400

    Added the topGO analysis for the rest of the combinations

--- results/Assn2_Q5/topGO/topgo_analysis.R
+++ results/Assn2_Q5/topGO/topgo_analysis.R
@@ -15,37 +15,52 @@ library(org.Hs.eg.db)
 library(dplyr)
 library(Rgraphviz)
 
-deg <- read.csv("differential_expression.csv")
+files <- c("diff_expression_t0_t24.csv",
+           "diff_expression_t0_t48.csv",
+           "diff_expression_t0_t72.csv",
+           "diff_expression_t24_t48.csv",
+           "diff_expression_t24_t72.csv",
+           "diff_expression_t48_t72.csv")
 
 alpha <- 0.05
-deg <- deg %>% mutate(isSig = ifelse(padj < alpha, 1, 0))
 
-geneList <- deg$isSig
-names(geneList) <- deg$symbol  # use gene symbols
-
-GOdata <- new("topGOdata",
-              description = "GO enrichment",
-              ontology = "BP",   # BP = Biological Process, MF, CC also possible
-              allGenes = geneList,
-              geneSel = function(x) x == 1,
-              nodeSize = 10,
-              annot = annFUN.org,
-              mapping = "org.Hs.eg.db",
-              ID = "symbol")
-
-resultFisher <- runTest(GOdata, algorithm = "classic", statistic = "fisher")
-resultFisher.elim <- runTest(GOdata, algorithm = "elim", statistic = "fisher")
-
-allRes <- GenTable(GOdata,
-                   classicFisher = resultFisher,
-                   elimFisher = resultFisher.elim,
-                   orderBy = "elimFisher",
-                   topNodes = 30)
-
-print(allRes)
-
-write.csv(allRes, file = "GO_enrichment_results.csv", row.names = FALSE)
-
-# Optional: visualize
-showSigOfNodes(GOdata, score(resultFisher), firstSigNodes = 5, useInfo = "all")
+for (f in files) {
+  deg <- read.csv(f)
+  
+  deg <- deg %>% mutate(isSig = ifelse(padj < alpha, 1, 0))
+  
+  geneList <- deg$isSig
+  names(geneList) <- deg$symbol  # use gene symbols
+  
+  GOdata <- new("topGOdata",
+                description = "GO enrichment",
+                ontology = "BP",   # BP = Biological Process, MF, CC also possible
+                allGenes = geneList,
+                geneSel = function(x) x == 1,
+                nodeSize = 10,
+                annot = annFUN.org,
+                mapping = "org.Hs.eg.db",
+                ID = "symbol")
+  
+  resultFisher <- runTest(GOdata, algorithm = "classic", statistic = "fisher")
+  resultFisher.elim <- runTest(GOdata, algorithm = "elim", statistic = "fisher")
+  
+  allRes <- GenTable(GOdata,
+                     classicFisher = resultFisher,
+                     elimFisher = resultFisher.elim,
+                     orderBy = "elimFisher",
+                     topNodes = 30)
+  
+  print(allRes)
+  
+  comp <- sub("diff_expression_(.*)\\.csv", "\\1", basename(f))
+  
+  out_file <- paste0("GO_enrichment_results_", comp, ".csv")
+  write.csv(allRes, file = out_file, row.names = FALSE)
+  
+  # Optional: visualize
+  pdf(paste0("GO_plot_", comp, ".pdf"))
+  showSigOfNodes(GOdata, score(resultFisher), firstSigNodes = 5, useInfo = "all")
+  dev.off()
+}
 

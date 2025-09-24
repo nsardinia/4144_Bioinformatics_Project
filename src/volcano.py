d30df4286c8a5fef3339ab48de43f69e94975aa8
ericg <ericgjinollari@gmail.com>
2025-09-24 13:42:39 -0400

    Colored Graphs comparing T0 to other time conditinons

--- src/volcano.py
+++ src/volcano.py
@@ -1,12 +1,18 @@
 import bioinfokit.visuz
-from diff_analysis import dif_analysis
+from diff_analysiscolor import dif_analysis
+import matplotlib.pyplot as plt
 
 
 def volcanoplot(datapath, metadatapah):
 
     da = dif_analysis(datapath, metadatapah)
 
-    bioinfokit.visuz.GeneExpression.volcano(df=da['t24'], lfc='log2FoldChange', pv='pvalue', show=True)
+    bioinfokit.visuz.GeneExpression.volcano(df=da['t24'], lfc='log2FoldChange', pv='pvalue', show=True, plotlegend=True,
+                                            sign_line=True, theme="dark")
+    bioinfokit.visuz.GeneExpression.volcano(df=da['t48'], lfc='log2FoldChange', pv='pvalue', show=True, plotlegend=True,
+                                            sign_line=True, theme="dark")
+    bioinfokit.visuz.GeneExpression.volcano(df=da['t72'], lfc='log2FoldChange', pv='pvalue', show=True, plotlegend=True,
+                                            sign_line=True, theme="dark")
 
 
 volcanoplot("data/SRP120552.tsv", "data/metadata_SRP120552.tsv")

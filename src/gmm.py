99ae64467250e42807a5c89004f5deaf409cf747
Nicholas Sardinia <nicholas_sardinia@outlook.com>
2025-10-15 17:10:15 -0400

    adjusted function format, generated additional plots

--- src/gmm.py
+++ src/gmm.py
@@ -4,12 +4,41 @@ import matplotlib.pyplot as plt
 from sklearn.decomposition import PCA
 from scipy.stats import chi2_contingency
 import seaborn as sns
+from typing import Optional
+
+
+def plotGMM(labels: list[int], save_path: Optional[str]="data/img/gmm_clusters.png") -> None:
+    plt.figure(figsize=(8, 6))
+    
+    # Histogram of cluster label frequencies
+    sns.countplot(x=labels)
+    
+    plt.title('Frequency of Cluster Labels')
+    plt.xlabel('Cluster Label')
+    plt.ylabel('Count')
+    plt.tight_layout()
+    plt.savefig(save_path, dpi=300)
 
 
 
-def gmm(datapath, k, num_of_genes):
-    df = pd.read_csv(datapath, sep="\t")
+#Use this for generic / further computation with the same standardized dataframe.
+def gmm(top_5000_df: pd.DataFrame, k:int, num_of_genes:int) -> list[int]:
+    #run preliminary PCA to reduce dimensionality before GMM (otherwise, too large)
+    if num_of_genes < 500:
+        pca = PCA(n_components=num_of_genes, random_state=42)
+    else:
+        pca = PCA(n_components=500, random_state=42)
+    X_reduced = pca.fit_transform(top_5000_df)
 
+
+    gmodel = GaussianMixture(n_components=k, random_state=42)
+    gmodel.fit(X_reduced)
+    labels = gmodel.predict(X_reduced)
+
+    return labels
+
+def processDF(num_of_genes: int) -> None:
+    df = pd.read_csv("data/with_gene_names.tsv", sep="\t")
     if df.columns[0].lower().startswith("ensg") or not pd.api.types.is_numeric_dtype(df.iloc[:, 0]):
         df = df.set_index(df.columns[0])
 
@@ -20,31 +49,35 @@ def gmm(datapath, k, num_of_genes):
     genes = gene_var.sort_values(ascending=False).head(num_of_genes).index
     data = numeric_data.loc[genes]
 
-    pca = PCA(n_components=50, random_state=42)
-    X_reduced = pca.fit_transform(data)
+    return data
 
-    gmodel = GaussianMixture(n_components=k, random_state=42)
-    gmodel.fit(X_reduced)
-    labels = gmodel.predict(X_reduced)
+#g_10 = plotGMM(gmm(pd.read_csv("data/with_gene_names.tsv", sep="\t"), 2, 10), "data/img/gmm_frq_10_2.png")
+g_100 = plotGMM(gmm(processDF(100), 2, 100), "data/img/gmm_frq_100_2.png")
+g_1000 = plotGMM(gmm(processDF(1000), 2, 1000), "data/img/gmm_frq_1000_2.png")
+g_5000 = plotGMM(gmm(processDF(5000), 2, 5000), "data/img/gmm_frq_5000_2.png")
+g_10000 = plotGMM(gmm(processDF(10000), 2, 10000), "data/img/gmm_frq_10000_2.png")
 
-    pca_vis = PCA(n_components=2, random_state=0)
-    X_2D = pca_vis.fit_transform(X_reduced)
+#g_10 = plotGMM(gmm(pd.read_csv("data/with_gene_names.tsv", sep="\t"), 4, 10), "data/img/gmm_frq_10_4.png")
+g_100 = plotGMM(gmm(processDF(100), 4, 100), "data/img/gmm_frq_100_4.png")
+g_1000 = plotGMM(gmm(processDF(1000), 4, 1000), "data/img/gmm_frq_1000_4.png")
+g_5000 = plotGMM(gmm(processDF(5000), 4, 5000), "data/img/gmm_frq_5000_4.png")
+g_10000 = plotGMM(gmm(processDF(10000), 4, 10000), "data/img/gmm_frq_10000_4.png")
 
-    plt.figure(figsize=(8, 6))
-    sns.scatterplot(x=X_2D[:, 0], y=X_2D[:, 1], hue=labels, palette='Set1', s=60, edgecolor='k')
-    plt.title('GMM Clustering (Visualized in 2D via PCA)')
-    plt.xlabel('PCA Component 1')
-    plt.ylabel('PCA Component 2')
-    plt.legend(title='Cluster')
-    plt.tight_layout()
-    plt.savefig("data/img/gmm_clusters10000.png", dpi=300)
+#g_10 = plotGMM(gmm(pd.read_csv("data/with_gene_names.tsv", sep="\t"), 8, 10), "data/img/gmm_frq_10_8.png")
+g_100 = plotGMM(gmm(processDF(100), 8, 100), "data/img/gmm_frq_100_8.png")
+g_1000 = plotGMM(gmm(processDF(1000), 8, 1000), "data/img/gmm_frq_1000_8.png")
+g_5000 = plotGMM(gmm(processDF(5000), 8, 5000), "data/img/gmm_frq_5000_8.png")
+g_10000 = plotGMM(gmm(processDF(10000), 8, 10000), "data/img/gmm_frq_10000_8.png")
 
+#g_10 = plotGMM(gmm(pd.read_csv("data/with_gene_names.tsv", sep="\t"), 16, 10), "data/img/gmm_frq_10_16.png")
+g_100 = plotGMM(gmm(processDF(100), 16, 100), "data/img/gmm_frq_100_16.png")
+g_1000 = plotGMM(gmm(processDF(1000), 16, 1000), "data/img/gmm_frq_1000_16.png")
+g_5000 = plotGMM(gmm(processDF(5000), 16, 5000), "data/img/gmm_frq_5000_16.png")
+g_10000 = plotGMM(gmm(processDF(10000), 16, 10000), "data/img/gmm_frq_10000_16.png")
 
-#g_10 = gmm("data/with_gene_names.tsv", 8, 10)
-#g_100 = gmm("data/with_gene_names.tsv", 8, 100)
-#g_1000 = gmm("data/with_gene_names.tsv", 1000)
-#g_5000 = gmm("data/with_gene_names.tsv", 8, 5000)
-g_10000 = gmm("data/with_gene_names.tsv", 8, 10000)
+#g_10 = plotGMM(gmm(pd.read_csv("data/with_gene_names.tsv", sep="\t"), 32, 10), "data/img/gmm_frq_10_32.png")
+g_100 = plotGMM(gmm(processDF(100), 32, 100), "data/img/gmm_frq_100_32.png")
+g_1000 = plotGMM(gmm(processDF(1000), 32, 1000), "data/img/gmm_frq_1000_32.png")
+g_5000 = plotGMM(gmm(processDF(5000), 32, 5000), "data/img/gmm_frq_5000_32.png")
+g_10000 = plotGMM(gmm(processDF(10000), 32, 10000), "data/img/gmm_frq_10000_32.png")
 
-
-#gmm() -> 

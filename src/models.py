f4b8b1f33cdc47977c53b797d3233eb320020574
Daniel Parra <daniel27parra@gmail.com>
2025-11-05 18:55:55 -0500

    started on part 3

---
+++ src/models.py
@@ -0,0 +1,20 @@
+import numpy as np
+
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.linear_model import LogisticRegression
+from sklearn.naive_bayes import GaussianNB
+
+def knn(x_train: np.ndarray, x_test: np.ndarray, y_train: np.ndarray) -> np.ndarray:
+    model = KNeighborsClassifier()
+    y_pred = model.fit(x_train, y_train).predict(x_test)
+    return y_pred
+
+def lr(x_train: np.ndarray, x_test: np.ndarray, y_train: np.ndarray) -> np.ndarray:
+    model = LogisticRegression()
+    y_pred = model.fit(x_train, y_train).predict(x_test)
+    return y_pred
+
+def gnb(x_train: np.ndarray, x_test: np.ndarray, y_train: np.ndarray) -> np.ndarray:
+    model = GaussianNB()
+    y_pred = model.fit(x_train, y_train).predict(x_test)
+    return y_pred
